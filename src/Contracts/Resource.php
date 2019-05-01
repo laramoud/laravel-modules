7ff9e0682d6e9cecd2ff0bdca60580c54480b4f2
Azam <pravodev@gmail.com>
2019-05-01 15:21:46 +0800

    load all resource (views, routes, migrations) of module

---
+++ src/Contracts/Resource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Pravodev\Laramoud\Contracts;
+use Illuminate\Support\Facades\Route;
+
+trait Resource
+{
+    /**
+     * Load View by namespace of module
+     * 
+     * @return void
+     */
+    public function loadViews($module_name)
+    {
+        $path = $this->getModulePath($module_name);
+        $namespace = $module_name;
+        $this->loadViewsFrom($path, $namespace);
+    }
+
+    /**
+     * Load Routes 
+     * 
+     * @return void
+     */
+    public function loadRoutes($module_name)
+    {
+        foreach(['web', 'api'] as $route_file){
+            $path = $this->getModulePath($module_name.'/routes/'.$route_file.'.php');
+            if(file_exists($path) == false) continue;
+            $namespace = $this->getNamespace($module_name, 'Http\Controllers');
+            $route = Route::middleware($route_file)->namespace($namespace);
+
+            if($route_file == 'api'){
+                $route->prefix($route_file);
+            }
+
+            $route->group($path);
+        }
+    }
+
+    /**
+     * Load Migrations 
+     */
+    public function loadMigrations($module_name)
+    {
+        $path = $this->getModulePath($module_name.'/database/migrations');
+        $this->loadMigrationsFrom($path, $namespace);
+    }
+
+    /**
+     * Get Module Namespace
+     * 
+     * @return string
+     */
+    public function getNamespace($module_name, $name = "")
+    {
+        $namespace = ucwords($module_name);
+        return $namespace.'\\'.$name;
+    }
+}
