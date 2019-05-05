42bd984e1598ae3dd86684369beb3cf8c859700e
Azam <pravodev@gmail.com>
2019-05-05 13:09:50 +0800

    signature

--- src/Commands/NewCommand.php
+++ src/Commands/NewCommand.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * This file is part of laramoud package.
+ * 
+ * @author Rifqi Khoeruman Azam <pravodev@gmail.com>
+ * 
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * Copyright © 2019 PondokIT. All rights reserved.
+ */
 
 namespace Pravodev\Laramoud\Commands;
 
@@ -64,8 +74,6 @@ class NewCommand extends Command
         $this->info('install module '.$this->argument('module_name'). '...');
         $this->line('composer require laramoud-module/'.$this->argument('module_name'));
         shell_exec('composer require laramoud-module/'.$this->argument('module_name'));
-            
-        
 
     }
 
--- src/Contracts/Module.php
+++ src/Contracts/Module.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of laramoud package.
+ * 
+ * @author Rifqi Khoeruman Azam <pravodev@gmail.com>
+ * 
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * Copyright © 2019 PondokIT. All rights reserved.
+ */
+
 namespace Pravodev\Laramoud\Contracts;
 
 use Pravodev\Laramoud\Utils\Cache;
